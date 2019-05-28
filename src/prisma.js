76663c4b4382b911d93d8a842c374f6aa3680542
Dimitris Zer <dimitris-007@hotmail.com>
2019-05-28 20:47:33 +0300

    updates

--- src/prisma.js
+++ src/prisma.js
@@ -2,7 +2,8 @@ const { Prisma } = require("prisma-binding");
 
 const prisma = new Prisma({
 	typeDefs: "src/generated/prisma.graphql",
-	endpoint: "https://news-server-prisma-d34d3feb97.herokuapp.com/news-prisma/dev"
+	//endpoint: "https://news-server-prisma-d34d3feb97.herokuapp.com/news-prisma/dev"
+	endpoint: "http://localhost:4466"
 });
 
 module.exports = prisma;
